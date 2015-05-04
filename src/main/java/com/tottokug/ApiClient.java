04aa4210dff475bc5f6d1d14d2b2c00bf1f162ee
aucfan-tokugami <tokugami@aucfan.com>
2015-05-04 09:48:34 +0900

    ClientAbstract Implementation

--- src/main/java/com/tottokug/ApiClient.java
+++ src/main/java/com/tottokug/ApiClient.java
@@ -1,13 +1,36 @@
 package com.tottokug;
 
+/**
+ * 
+ * @author tokugami
+ *
+ */
 public interface ApiClient {
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getApiKey();
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getApiEndpoint();
 
+	/**
+	 * 
+	 * @param request
+	 * @return
+	 */
 	public AzureMLResponse request(AzureMLRequest request);
 
+	/**
+	 * 
+	 * @param requset
+	 * @return
+	 */
 	public AzureMLBatchResponse requestBatch(AzureMLBatchRequest requset);
 
 }
--- src/main/java/com/tottokug/ApiClientAbstract.java
+++ src/main/java/com/tottokug/ApiClientAbstract.java
@@ -1,12 +1,31 @@
 package com.tottokug;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.MethodNotSupportedException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 
+ * @author tokugami
+ *
+ */
 public class ApiClientAbstract implements ApiClient {
 
 	static final Logger logger = LoggerFactory.getLogger(ApiClientAbstract.class);
@@ -14,32 +33,74 @@ public class ApiClientAbstract implements ApiClient {
 	private String apikey;
 	private String endpoint;
 
+	private ResponseHandler<AzureMLResponse> responseHandler;
+
+	private HttpContext httpContext;
+
+	/**
+	 * @author tokugami
+	 */
+	public ApiClientAbstract() {
+		responseHandler = new ResponseHandler<AzureMLResponse>() {
+
+			@Override
+			public AzureMLResponse handleResponse(HttpResponse arg0) throws ClientProtocolException, IOException {
+				return null;
+			}
+
+		};
+	}
+
+	/**
+	 * @author tokugami
+	 */
 	public String getApiKey() {
 
 		return this.apikey;
 	}
 
+	/**
+	 * @author tokugami
+	 */
 	public String getApiEndpoint() {
 		return this.endpoint;
 	}
 
+	/**
+	 * @author tokugami
+	 * @return
+	 * @param
+	 */
 	public AzureMLResponse request(AzureMLRequest request) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public AzureMLBatchResponse requestBatch(AzureMLBatchRequest requset) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public void requestHttp() {
-		try (CloseableHttpClient httpClient = HttpClientBuilder.create().disableAuthCaching().build()) {
+	public AzureMLResponse requestHttp(AzureMLRequest request) throws URISyntaxException, MethodNotSupportedException {
+		List<Header> defaultHeaders = Collections.synchronizedList(new ArrayList<Header>());
 
+		try (CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultHeaders(defaultHeaders)
+				.disableAuthCaching().build()) {
+			HttpHost httpHost;
+			httpHost = new HttpHost(this.getHostName());
+
+			HttpRequest httpRequest;
+			httpRequest = new HttpPost();
+
+			return httpClient.execute(httpHost, httpRequest, responseHandler, httpContext);
 		} catch (IOException e) {
 			logger.trace(e.getMessage(), e);
 			logger.error(e.getMessage());
 		}
+		return null;
+	}
+
+	private String getHostName() throws URISyntaxException {
+		URI uri = new URI(this.endpoint);
+		return uri.getHost();
 	}
 
 }
